457b7c83bba803bb8344cf45f48f0e42454585e8
Saúl Arteaga <alojera15@gmail.com>
2023-05-30 11:08:19 +0100

    feat(character): added constructor

--- src/net/game/classes/character/Character.java
+++ src/net/game/classes/character/Character.java
@@ -1,5 +1,11 @@
 package net.game.classes.character;
 
 public class Character {
-    
+    Integer health, defense, strength;
+
+    public Character(Integer defense, Integer strength) {
+        this.health = 12;
+        this.defense = defense;
+        this.strength = strength;
+    }
 }
